e90f9eabb3da2f60bbb3eadf1cf6fa03c33a645e
Kanika <kanika13c@gmail.com>
2026-06-03 18:17:43 +0530

    Fix production api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://connectly-backend-l4i2.onrender.com",
+  baseURL: "https://connectly-backend-l4i2.onrender.com/api",
 });
 
 export default API;
